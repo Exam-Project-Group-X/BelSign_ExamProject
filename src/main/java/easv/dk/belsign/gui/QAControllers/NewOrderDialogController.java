4bfa646b81880d685ecbfb56a1f4501e24a009f9
Jianming Fan <151937346+Jianming2024@users.noreply.github.com>
2025-04-30 14:53:01 +0200

    Revise icons on QACard fxml file .

--- src/main/java/easv/dk/belsign/gui/QAControllers/NewOrderDialogController.java
+++ src/main/java/easv/dk/belsign/gui/QAControllers/NewOrderDialogController.java
@@ -1,16 +1,7 @@
-package easv.dk.belsign.gui.QAControllers.components;
+package easv.dk.belsign.gui.QAControllers;
 
-import easv.dk.belsign.gui.QAControllers.QAEmployeeController;
-import easv.dk.belsign.gui.ViewManagement.FXMLPath;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class NewOrderDialogController {
 
--- src/main/java/easv/dk/belsign/gui/QAControllers/QAEmployeeController.java
+++ src/main/java/easv/dk/belsign/gui/QAControllers/QAEmployeeController.java
@@ -1,18 +1,12 @@
 package easv.dk.belsign.gui.QAControllers;
 
-import easv.dk.belsign.gui.QAControllers.components.NewOrderDialogController;
 import easv.dk.belsign.gui.ViewManagement.FXMLPath;
-import easv.dk.belsign.gui.ViewManagement.ViewManager;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
-import javafx.scene.layout.GridPane;
-import javafx.scene.control.Label;
 import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
-import javafx.scene.control.ComboBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
