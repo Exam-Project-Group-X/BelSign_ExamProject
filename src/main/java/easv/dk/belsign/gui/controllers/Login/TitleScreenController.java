3bc524b82b126f7d82394ae847c805a11b0d17ed
Jianming Fan <151937346+Jianming2024@users.noreply.github.com>
2025-05-04 15:34:04 +0200

    Improve TitleScreen log in view

--- src/main/java/easv/dk/belsign/gui/controllers/Login/TitleScreenController.java
+++ src/main/java/easv/dk/belsign/gui/controllers/Login/TitleScreenController.java
@@ -2,11 +2,33 @@ package easv.dk.belsign.gui.controllers.Login;
 
 import easv.dk.belsign.gui.ViewManagement.FXMLPath;
 import easv.dk.belsign.gui.ViewManagement.ViewManager;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.SwipeEvent;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
 public class TitleScreenController {
+    @FXML
+    private Rectangle swipeRect;
+
+    public void initialize() {
+        animateRectangle();
+    }
+
+    // Animates the rectangle moving up and down continuously.
+    private void animateRectangle() {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(1.5), swipeRect);
+        transition.setFromY(0);
+        transition.setToY(-20);
+        transition.setCycleCount(TranslateTransition.INDEFINITE);
+        transition.setAutoReverse(true);
+        transition.play();
+    }
+
     public void onClickScanBtn(ActionEvent actionEvent) {
         ViewManager.INSTANCE.showScene(FXMLPath.OPERATOR_DASHBOARD);
         System.out.println("scane btn detected! Navigating to operator View.");
