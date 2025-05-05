adc00bde6438e95b4ce3f7b9fe1dda7e14d6c44e
Jianming Fan <151937346+Jianming2024@users.noreply.github.com>
2025-05-05 12:48:11 +0200

    change status color on QA view

--- src/main/java/easv/dk/belsign/gui/controllers/QAEmployee/OrderCardController.java
+++ src/main/java/easv/dk/belsign/gui/controllers/QAEmployee/OrderCardController.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 
 
 public class OrderCardController {
+    @FXML private Label statusLabel;
     @FXML private Label orderNumberLabel;
     @FXML private Label descriptionLabel;
     @FXML private Label createdAtLabel;
@@ -36,7 +37,17 @@ public class OrderCardController {
         orderNumberLabel.setText(order.getOrderNumber());
         descriptionLabel.setText(order.getProductDescription() == null ? "No description" : order.getProductDescription());
         createdAtLabel.setText(order.getCreatedAt() != null ? order.getCreatedAt().toString() : "Now");
-
+        String status = order.getOrderStatus();
+        statusLabel.setText(status);
+        if ("New".equals(status)) {
+            statusLabel.setStyle("-fx-background-color: #F7EAEA; -fx-text-fill: #E57373;");
+        } else if ("Pending".equals(status)) {
+            statusLabel.setStyle("-fx-background-color: #F7F3EA; -fx-text-fill: #F4B400;");
+        } else if ("Complete".equals(status)) {
+            statusLabel.setStyle("-fx-background-color: #EAF7F3; -fx-text-fill: #35B587;");
+        } else {
+            statusLabel.setStyle("");
+        }
         // Load photos for the order
         if (photoGridController != null) {
             photoGridController.loadPhotosForOrder(order.getOrderID());
