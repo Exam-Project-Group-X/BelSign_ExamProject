bdcd39c8d87f5d40a02fe8855c1e91f39967d7de
Lucaz2010 <oliveirasantos.lucas.sm@gmail.com>
2025-05-08 13:12:34 +0200

    Unnecessary methods - dao

--- src/main/java/easv/dk/belsign/dal/web/ProductPhotosDAO.java
+++ src/main/java/easv/dk/belsign/dal/web/ProductPhotosDAO.java
@@ -56,46 +56,4 @@ public class ProductPhotosDAO {
         }
     }
 
-
-    public void insertTestPhoto(String filePath, int orderId, String angle) throws Exception {
-        byte[] bytes;
-        try {
-            bytes = Files.readAllBytes(Path.of(filePath));
-        } catch (IOException e) {
-            System.err.println("❌ Failed to load: " + filePath);
-            e.printStackTrace();
-            return; // Exit early if reading failed
-        }
-
-        String sql = "INSERT INTO ProductPhotos (OrderID, PhotoAngle, Status, PhotoData) VALUES (?, ?, ?, ?)";
-
-        try (Connection conn = con.getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql)) {
-
-            ps.setInt(1, orderId);
-            ps.setString(2, angle);
-            ps.setString(3, "Pending");
-            ps.setBytes(4, bytes);
-            ps.executeUpdate();
-            System.out.println("✅ Inserted photo for " + angle + orderId);
-        }
-    }
-
-    public void insertAllTestPhotos() throws Exception {
-//        insertTestPhoto("src/main/resources/easv/dk/belsign/testphotos/bulbasaur.jpg", 1, "FRONT");
-//        insertTestPhoto("src/main/resources/easv/dk/belsign/testphotos/charmander.jpg", 1, "BACK");
-//        insertTestPhoto("src/main/resources/easv/dk/belsign/testphotos/squirtle.png", 1, "LEFT");
-//        insertTestPhoto("src/main/resources/easv/dk/belsign/testphotos/pikachu.jpg", 1, "RIGHT");
-//        insertTestPhoto("src/main/resources/easv/dk/belsign/testphotos/eevee.jpg", 1, "TOP");
-//        insertTestPhoto("src/main/resources/easv/dk/belsign/testphotos/snorlax.png", 1, "BOTTOM");
-
-        insertTestPhoto("src/main/resources/easv/dk/belsign/testphotos/test1.jpeg", 2, "FRONT");
-        insertTestPhoto("src/main/resources/easv/dk/belsign/testphotos/test2.jpeg", 2, "BACK");
-        insertTestPhoto("src/main/resources/easv/dk/belsign/testphotos/test3.jpeg", 2, "LEFT");
-        insertTestPhoto("src/main/resources/easv/dk/belsign/testphotos/test4.jpeg", 2, "RIGHT");
-        insertTestPhoto("src/main/resources/easv/dk/belsign/testphotos/test5.jpeg", 2, "TOP");
-        insertTestPhoto("src/main/resources/easv/dk/belsign/testphotos/test6.jpeg", 2, "BOTTOM");
-
-    }
-
 }
