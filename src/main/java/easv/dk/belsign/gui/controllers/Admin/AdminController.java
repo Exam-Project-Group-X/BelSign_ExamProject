a3d9f7e14e5e02611b9751f8ad1c294f09c8811e
Jianming Fan <151937346+Jianming2024@users.noreply.github.com>
2025-05-14 16:40:45 +0200

    Add pagination on Admin and QA views

--- src/main/java/easv/dk/belsign/gui/controllers/Admin/AdminController.java
+++ src/main/java/easv/dk/belsign/gui/controllers/Admin/AdminController.java
@@ -1,4 +1,5 @@
 package easv.dk.belsign.gui.controllers.Admin;
+import easv.dk.belsign.be.Order;
 import easv.dk.belsign.be.User;
 import easv.dk.belsign.gui.ViewManagement.FXMLPath;
 import easv.dk.belsign.gui.ViewManagement.ViewManager;
@@ -28,23 +29,83 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.ResourceBundle;
 public class AdminController implements Initializable {
-
+    @FXML
+    private HBox toggleBtnContainer;
+    @FXML
+    private Button firstPageBtn;
+    @FXML
+    private Button lastPageBtn;
+    @FXML
+    private Label lblPageInfo;
+    @FXML
+    private Button prevPageBtn;
+    @FXML
+    private Button nextPageBtn;
     @FXML private ComboBox<String> statusFilter;
     @FXML private TextField searchField;
     @FXML private VBox cardContainer;
 
+    private static final int PAGE_SIZE = 6;
+    private static final int TOGGLE_COUNT = 5;// cards per page
+    private int currentPage = 1;
+    private int pageCount  = 1;
+    private ToggleGroup toggleGroup = new ToggleGroup();
     private static final UserModel userModel = new UserModel();
     private List<User> allUsersList;
+
     @Override
-
     public void initialize(URL location, ResourceBundle resources) {
-
         try {
+            // Get all users from the UserModel
+            allUsersList = userModel.getAllUsers();
+            loadPage(currentPage);
+            pageCount = (int)Math.ceil((double)allUsersList.size() / PAGE_SIZE);
+            //updatePageCount(allUsersList.size());
+            updatePaginationToggles();
             loadAllUsers();
             searchField.textProperty().addListener((obs, oldVal, newVal) -> filterUsers());
             statusFilter.valueProperty().addListener((obs, oldVal, newVal) -> filterUsers());
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private void updatePageCount(int totalItems) {
+        pageCount = (int) Math.ceil(totalItems / (double) PAGE_SIZE);
+
+        if (currentPage > pageCount) currentPage = pageCount;
+    }
+
+    public void loadPage(int page) {
+        cardContainer.getChildren().clear();
+        int start = (page - 1) * PAGE_SIZE;
+        int end = Math.min(start + PAGE_SIZE, allUsersList.size());
+        for (int i = start; i < end; i++) {
+            addUserCard(allUsersList.get(i));
+        }
+        lblPageInfo.setText("Showing page " + currentPage + " of " + pageCount);
+    }
+
+    // Dynamically generate pagination toggle buttons based on current page
+    private void updatePaginationToggles() {
+        toggleBtnContainer.getChildren().clear();
+        int startPage = ((currentPage - 1) / TOGGLE_COUNT) * TOGGLE_COUNT + 1;
+        int endPage = Math.min(startPage + TOGGLE_COUNT - 1, pageCount);
+
+        for (int i = startPage; i <= endPage; i++) {
+            ToggleButton toggle = new ToggleButton(String.valueOf(i));
+            toggle.setToggleGroup(toggleGroup);
+            if (i == currentPage) {
+                toggle.setStyle("-fx-background-color: #004884; -fx-text-fill: white;");
+                toggle.setSelected(true);
+            }
+            int pageNum = i;
+            toggle.setOnAction(e -> {
+                currentPage = pageNum;
+                loadPage(currentPage);
+                updatePaginationToggles();
+            });
+            toggleBtnContainer.getChildren().add(toggle);
         }
     }
 
@@ -135,5 +196,33 @@ public class AdminController implements Initializable {
         for (User user : filteredUsers) {
             addUserCard(user);
         }
+    }
+
+    public void onClickFirstPageBtn(ActionEvent actionEvent) {
+        currentPage = 1;
+        loadPage(currentPage);
+        updatePaginationToggles();
+    }
+
+    public void onClickPrevPageBtn(ActionEvent actionEvent) {
+        if (currentPage > 1) {
+            currentPage--;
+            loadPage(currentPage);
+            updatePaginationToggles();
+        }
+    }
+
+    public void onCLickNextPageBtn(ActionEvent actionEvent) {
+        if (currentPage < pageCount) {
+            currentPage++;
+            loadPage(currentPage);
+            updatePaginationToggles();
+        }
+    }
+
+    public void onClickLastPageBtn(ActionEvent actionEvent) {
+        currentPage = pageCount;
+        loadPage(currentPage);
+        updatePaginationToggles();
     }
 }
--- src/main/java/easv/dk/belsign/gui/controllers/QAEmployee/QAEmployeeController.java
+++ src/main/java/easv/dk/belsign/gui/controllers/QAEmployee/QAEmployeeController.java
@@ -7,17 +7,29 @@ import easv.dk.belsign.gui.ViewManagement.ViewManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.Button;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.List;
+import java.util.ResourceBundle;
 
-public class QAEmployeeController {
+public class QAEmployeeController implements Initializable {
     @FXML
-    private Label pageInfoLabel;
+    private HBox toggleBtnContainer;
+    @FXML
+    private Button firstPageBtn;
+    @FXML
+    private Button lastPageBtn;
+    @FXML
+    private Label lblPageInfo;
     @FXML
     private Button prevPageBtn;
     @FXML
@@ -29,41 +41,29 @@ public class QAEmployeeController {
     @FXML
     private Button logoutButton;
 
-    private static final int PAGE_SIZE = 3;                    // cards per page
+    private static final int PAGE_SIZE = 3;
+    private static final int TOGGLE_COUNT = 5;// cards per page
     private int currentPage = 1;
     private int pageCount  = 1;
     private List<Order> orders;
-
+    private ToggleGroup toggleGroup = new ToggleGroup();
     private final QAEmployeeModel qamodel = new QAEmployeeModel();
-    @FXML
-    public void initialize() {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
         try {
             // Get all orders from the QAEmployeeModel
             orders = qamodel.getAllOrders();
             pageCount = (int)Math.ceil((double)orders.size() / PAGE_SIZE);
             loadPage(currentPage);
+            updatePaginationToggles();
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
-        prevPageBtn.setOnAction(event -> {
-            if (currentPage > 1) {
-                currentPage--;
-                loadPage(currentPage);
-            }
-        });
-
-        nextPageBtn.setOnAction(event -> {
-            if (currentPage < pageCount) {
-                currentPage++;
-                loadPage(currentPage);
-            }
-        });
     }
 
-/// Generate QC Report button only clickable after approving ALL photos (i.e. Status "Complete"
-///  -> Then you can Generate QC Report)
+    /// Generate QC Report button only clickable after approving ALL photos (i.e. Status "Complete"
+    ///  -> Then you can Generate QC Report)
     public void loadPage(int page) {
         cardContainer.getChildren().clear();
         int start = (page - 1) * PAGE_SIZE;
@@ -71,7 +71,7 @@ public class QAEmployeeController {
         for (int i = start; i < end; i++) {
             addNewOrderCard(orders.get(i));
         }
-        pageInfoLabel.setText("Showing page " + currentPage + " of " + pageCount);
+        lblPageInfo.setText("Showing page " + currentPage + " of " + pageCount);
     }
 
     public void addNewOrderCard(Order order) {
@@ -89,10 +89,62 @@ public class QAEmployeeController {
             e.printStackTrace();
         }
     }
+    // Dynamically generate pagination toggle buttons based on current page
+    private void updatePaginationToggles() {
+        toggleBtnContainer.getChildren().clear();
+        int startPage = ((currentPage - 1) / TOGGLE_COUNT) * TOGGLE_COUNT + 1;
+        int endPage = Math.min(startPage + TOGGLE_COUNT - 1, pageCount);
+
+        for (int i = startPage; i <= endPage; i++) {
+            ToggleButton toggle = new ToggleButton(String.valueOf(i));
+            toggle.setToggleGroup(toggleGroup);
+            if (i == currentPage) {
+                toggle.setStyle("-fx-background-color: #004884; -fx-text-fill: white;");
+                toggle.setSelected(true);
+            }
+            int pageNum = i;
+            toggle.setOnAction(e -> {
+                currentPage = pageNum;
+                loadPage(currentPage);
+                updatePaginationToggles();
+            });
+            toggleBtnContainer.getChildren().add(toggle);
+        }
+    }
 
     public void onClickLogoutBtn(ActionEvent actionEvent) {
         ViewManager.INSTANCE.showScene(FXMLPath.LOGIN);
     }
+
+    public void onClickFirstPageBtn(ActionEvent actionEvent) {
+        currentPage = 1;
+        loadPage(currentPage);
+        updatePaginationToggles();
+    }
+
+    public void onClickPrevPageBtn(ActionEvent actionEvent) {
+        if (currentPage > 1) {
+            currentPage--;
+            loadPage(currentPage);
+            updatePaginationToggles();
+        }
+    }
+
+    public void onCLickNextPageBtn(ActionEvent actionEvent) {
+        if (currentPage < pageCount) {
+            currentPage++;
+            loadPage(currentPage);
+            updatePaginationToggles();
+        }
+    }
+
+    public void onClickLastPageBtn(ActionEvent actionEvent) {
+        currentPage = pageCount;
+        loadPage(currentPage);
+        updatePaginationToggles();
+    }
+
+
 }
 
 
