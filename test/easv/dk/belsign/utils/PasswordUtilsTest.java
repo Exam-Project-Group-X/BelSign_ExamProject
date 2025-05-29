9e94085512ebb26b69a9843dd25970baa5fb1205
Jianming Fan <151937346+Jianming2024@users.noreply.github.com>
2025-05-30 01:27:39 +0200

    Add one more unit testing for password checking

---
+++ test/easv/dk/belsign/utils/PasswordUtilsTest.java
@@ -0,0 +1,49 @@
+package easv.dk.belsign.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordUtilsTest {
+
+    @Test
+    void hashProducesDifferentValueThanPlainText() {
+        String pwd = "MySecr3t!";
+        String hash = PasswordUtils.hashPassword(pwd);
+
+        assertNotNull(hash, "HashCode should not be null");
+        assertNotEquals(pwd, hash, "HashCode must not equal the plain‐text password");
+    }
+
+    @Test
+    void verifyAcceptsCorrectPassword() {
+        String pwd = "Another$Pass123";
+        String hash = PasswordUtils.hashPassword(pwd);
+
+        assertTrue(PasswordUtils.checkPassword(pwd, hash),
+                "verify() should return true for the original password");
+    }
+
+    @Test
+    void verifyRejectsWrongPassword() {
+        String pwd     = "CorrectHorseBatteryStaple";
+        String hash    = PasswordUtils.hashPassword(pwd);
+        String badP    = "correcthorsebatterystaple";  // different case
+
+        assertFalse(PasswordUtils.checkPassword(badP, hash),
+                "verify() should return false for an incorrect password");
+    }
+
+    @Test
+    void hashIsNonDeterministic() {
+        String pwd = "RepeatMe!";
+        String h1  = PasswordUtils.hashPassword(pwd);
+        String h2  = PasswordUtils.hashPassword(pwd);
+
+        assertNotEquals(h1, h2,
+                "Each call to hash() should produce a different salt, hence a different hash");
+        // But both still verify:
+        assertTrue(PasswordUtils.checkPassword(pwd, h1));
+        assertTrue(PasswordUtils.checkPassword(pwd, h2));
+    }
+}
